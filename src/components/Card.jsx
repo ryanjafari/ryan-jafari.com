508a1245eb68baa8ed8b20853a5905cd6ba70882
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-01-09 21:22:24 +0000

    Refactor Card component styles
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,16 +4,16 @@ import Link from 'next/link'
 function ChevronRightIcon(props) {
   return (
     <svg
-      viewBox="0 0 16 16"
-      fill="none"
       aria-hidden="true"
+      fill="none"
+      viewBox="0 0 16 16"
       {...props}
     >
       <path
         d="M6.75 5.75 9.25 8l-2.5 2.25"
-        strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
+        strokeWidth="1.5"
       />
     </svg>
   )
@@ -34,7 +34,7 @@ export function Card({ as, className, children }) {
 Card.Link = function CardLink({ children, ...props }) {
   return (
     <>
-      <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
+      <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50" />
       <Link {...props}>
         <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
         <span className="relative z-10">{children}</span>
@@ -55,7 +55,7 @@ Card.Title = function CardTitle({ as, href, children }) {
 
 Card.Description = function CardDescription({ children }) {
   return (
-    <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400 sm:line-clamp-3 sm:h-[4.5rem] md:line-clamp-2 md:h-[3rem]">
+    <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
       {children}
     </p>
   )
@@ -93,8 +93,8 @@ Card.Eyebrow = function CardEyebrow({
     >
       {decorate && (
         <span
-          className="absolute inset-y-0 left-0 flex items-center"
           aria-hidden="true"
+          className="absolute inset-y-0 left-0 flex items-center"
         >
           <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
         </span>
