b8e30867798191c923c6c31d7c329c0423515134
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-01-23 16:52:52 +0000

    Refactor formatDate function to handle different date input types
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- src/lib/formatDate.js
+++ src/lib/formatDate.js
@@ -1,8 +1,28 @@
-export function formatDate(dateString) {
-  // Check if the input dateString is in 'YYYY-MM-DD' format
-  const isFullDate = /^\d{4}-\d{2}-\d{2}$/.test(dateString)
+export function formatDate(dateInput) {
+  let dateString
 
-  if (isFullDate) {
+  // Check if dateInput is a Date object
+  if (dateInput instanceof Date) {
+    dateString = dateInput.toISOString().split('T')[0]
+  } else if (typeof dateInput === 'string') {
+    dateString = dateInput
+  } else {
+    console.error('Invalid date type:', dateInput)
+    return ''
+  }
+
+  // Check if the dateString is in 'YYYY-MM-DD' or 'YYYY-MM-DDTHH:mm:ss.sssZ' format
+  const isFullDate = /^\d{4}-\d{2}-\d{2}$/.test(dateString)
+  const isDateTimeFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(
+    dateString,
+  )
+
+  if (isFullDate || isDateTimeFormat) {
+    // If it's in the full datetime format, extract the date part only
+    if (isDateTimeFormat) {
+      dateString = dateString.split('T')[0]
+    }
+
     return new Date(`${dateString}T00:00:00Z`).toLocaleDateString('en-US', {
       day: 'numeric',
       month: 'long',
@@ -10,7 +30,7 @@ export function formatDate(dateString) {
       timeZone: 'UTC',
     })
   } else {
-    // Check if the input dateString is in 'YYYY' format
+    // Check if the dateString is in 'YYYY' format
     const isYearOnly = /^\d{4}$/.test(dateString)
     if (isYearOnly) {
       return dateString
