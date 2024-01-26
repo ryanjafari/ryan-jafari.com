125c271f5b0a7d72247cc07eaeafc4c7f3663082
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-01-26 18:29:37 +0000

    [CK-Broadcast] src/app/articles/automating-convertkit-broadcasts-with-github-actions/page.mdx
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/check_commit_for_broadcast.sh
+++ .github/workflows/check_commit_for_broadcast.sh
@@ -2,7 +2,7 @@
 
 commit_message=$(git log -1 --pretty=%B)
 
-printf "The commit message is:\n\"%s\"\n" "$commit_message"
+# printf "The commit message is:\n\"%s\"\n" "$commit_message"
 
 if [[ "$commit_message" =~ ^\[CK-Broadcast\]\ (src/app/articles/[^[:space:]]+\.(md|mdx))([[:space:]]|$) ]]; then
   article_path=${BASH_REMATCH[1]}
@@ -15,5 +15,6 @@ if [[ "$commit_message" =~ ^\[CK-Broadcast\]\ (src/app/articles/[^[:space:]]+\.(
     echo "File does not exist: $article_path."
   fi
 else
-  echo "No valid article to broadcast!"
+  article_path=${BASH_REMATCH[1]}
+  echo "No valid article to broadcast: $article_path!"
 fi
