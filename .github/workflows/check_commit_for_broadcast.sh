5657218fd4b6659cec9128f9d2014c2ecbcc1df1
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-01-26 18:57:59 +0000

    [CK-Broadcast] src/app/articles/automating-convertkit-broadcasts-with-github-actions/page.jpg
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/check_commit_for_broadcast.sh
+++ .github/workflows/check_commit_for_broadcast.sh
@@ -2,16 +2,14 @@
 
 commit_message=$(git log -1 --pretty=%B)
 
-# printf "The commit message is:\n\"%s\"\n" "$commit_message"
-
 if [[ "$commit_message" =~ ^\[CK-Broadcast\]\ (src/app/articles/[^[:space:]]+\.[^[:space:]]+)([[:space:]]|$) ]]; then
-  article_path=${BASH_REMATCH[1]}
+  article_path="${BASH_REMATCH[1]}"
   echo "[CK-Broadcast] Got article path: $article_path"
 
   if [[ "$article_path" == *.md || "$article_path" == *.mdx ]]; then
     echo "[CK-Broadcast] Valid file type: $article_path"
 
-    if [[ -f $article_path ]]; then
+    if [[ -f "$article_path" ]]; then
       echo "[CK-Broadcast] File exists: $article_path"
       echo "article_path=$article_path" >>"$GITHUB_OUTPUT"
     else
@@ -21,6 +19,11 @@ if [[ "$commit_message" =~ ^\[CK-Broadcast\]\ (src/app/articles/[^[:space:]]+\.[
     echo "[CK-Broadcast] Invalid file type: $article_path"
   fi
 else
-  echo "[CK-Broadcast] Commit message did not match: $commit_message"
-  echo "[CK-Broadcast] Expected format: [CK-Broadcast] src/app/articles/<article-name>.md(x)"
+  echo "[CK-Broadcast] Commit message did not match expected format."
+  echo "[CK-Broadcast] Expected format: [CK-Broadcast] src/app/articles/<article-name>.md(x) <commit-message>"
+fi
+
+if [[ -z "$article_path" ]]; then
+  echo "[CK-Broadcast] No article path found. Will not broadcast."
+  echo "article_path=" >>"$GITHUB_OUTPUT"
 fi
