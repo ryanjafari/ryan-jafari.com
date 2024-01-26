a617c7562e236ae3d3d59c63ee641e6bf0127578
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-01-26 20:38:43 +0000

    [CK-Broadcast] src/app/articles/automating-convertkit-broadcasts-with-github-actions/page.mdx
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/send_ck_email.sh
+++ .github/workflows/send_ck_email.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-article_data=$(node ./.github/worfklows/parseFrontMatter.js "$ARTICLE_PATH")
-IFS='|' read -r article_date article_title article_description <<<"$article_data"
-echo "[CK-Broadcast] Article data: $article_data"
+echo "$ARTICLE_DATA"
 
-IFS='|' read -r article_date article_title article_description <<<"$article_data"
-echo "[CK-Broadcast] Article date: $article_date"
-echo "[CK-Broadcast] Article title: $article_title"
-echo "[CK-Broadcast] Article description: $article_description"
+# article_data=$(node ./.github/worfklows/parseFrontMatter.js "$ARTICLE_PATH")
+# IFS='|' read -r article_date article_title article_description <<<"$article_data"
+# echo "[CK-Broadcast] Article data: $article_data"
 
-# Prepare the email content
-email_subject="New Blog Post: $article_title"
-email_body="<p>Published on: $article_date</p><p>$article_description</p>"
+# IFS='|' read -r article_date article_title article_description <<<"$article_data"
+# echo "[CK-Broadcast] Article date: $article_date"
+# echo "[CK-Broadcast] Article title: $article_title"
+# echo "[CK-Broadcast] Article description: $article_description"
 
-# Construct the API endpoint URL
-ck_api_endpoint="${CK_API_BASE_URL}${CK_API_BROADCASTS_ENDPOINT}"
+# # Prepare the email content
+# email_subject="New Blog Post: $article_title"
+# email_body="<p>Published on: $article_date</p><p>$article_description</p>"
 
-# ConvertKit API call
-curl -X POST "$ck_api_endpoint" \
-  -H "Content-Type: application/json" \
-  -d '{
-        "api_key": "'"$CK_API_KEY"'",
-        "subject": "'"$email_subject"'",
-        "body": "'"$email_body"'"
-      }'
+# # Construct the API endpoint URL
+# ck_api_endpoint="${CK_API_BASE_URL}${CK_API_BROADCASTS_ENDPOINT}"
+
+# # ConvertKit API call
+# curl -X POST "$ck_api_endpoint" \
+#   -H "Content-Type: application/json" \
+#   -d '{
+#         "api_key": "'"$CK_API_KEY"'",
+#         "subject": "'"$email_subject"'",
+#         "body": "'"$email_body"'"
+#       }'
