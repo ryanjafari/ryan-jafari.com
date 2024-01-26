e418cdefd50f2c8147b99d6e30da017459447de3
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-01-26 17:50:39 +0000

    [CK-Broadcast] src/app/articles/automating-convertkit-broadcasts-with-github-actions/page.mdxx
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/check_commit_for_broadcast.sh
+++ .github/workflows/check_commit_for_broadcast.sh
@@ -2,8 +2,12 @@
 
 commit_message=$(git log -1 --pretty=%B)
 
+echo "The commit message is: $commit_message"
+
 if [[ "$commit_message" =~ ^\[CK-Broadcast\]\ (.+)$ ]]; then
   article_path=${BASH_REMATCH[1]}
+  echo "$article_path"
+
   if [[ -f $article_path ]]; then
     echo "article_path=$article_path" >>"$GITHUB_OUTPUT"
     echo "Will broadcast article: $article_path..."
