5e56a241395c4c43979f892266192bb5922cbf7d
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-01-31 23:33:21 +0000

    [CK-Broadcast] src/app/articles/alopecia-universalis/page.md
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/parseFrontMatter.js
+++ .github/workflows/parseFrontMatter.js
@@ -1,11 +1,11 @@
 import chalk from 'chalk'
-import fs from 'fs'
+import fsp from 'fs/promises'
 import yaml from 'js-yaml'
 
 export default async function parseFrontMatter({ github, context }) {
   const { ARTICLE_PATH } = process.env
 
-  const content = fs.readFile(ARTICLE_PATH, 'utf8')
+  const content = await fsp.readFile(ARTICLE_PATH, 'utf8')
   const parts = content.split('---')
   const frontMatter = yaml.load(parts[1])
 
