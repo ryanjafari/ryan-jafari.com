27790a2ac9ff654c7e5cbd967432fac77e339eaa
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-02-01 00:10:02 +0000

    [CK-Broadcast] src/app/articles/alopecia-universalis/page.md
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/parseFrontMatter.js
+++ .github/workflows/parseFrontMatter.js
@@ -9,17 +9,15 @@ function customLog(...args) {
 }
 
 export default async function parseFrontMatter({ github, context }) {
+  customLog(chalk.yellow('Parsing article front matter...'))
+
   const { ARTICLE_PATH } = process.env
+  customLog(chalk.blue('article path:'), ARTICLE_PATH)
 
   const content = await fsp.readFile(ARTICLE_PATH, 'utf8')
   const parts = content.split('---')
   const frontMatter = yaml.load(parts[1])
-
-  customLog(chalk.blue('Article front matter:'), frontMatter)
-
-  // console.log(
-  //   `[CK-Broadcast] ${frontMatter.date}|${frontMatter.title}|${frontMatter.description}`,
-  // )
+  customLog(chalk.blue('article front matter:'), frontMatter)
 
   return frontMatter
 }
