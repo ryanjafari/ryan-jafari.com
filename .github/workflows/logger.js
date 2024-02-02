b2eb138a625adc058dd1c78510b7fc8644052a52
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-02-02 20:25:04 +0000

    [CK-Broadcast] src/app/articles/alopecia-universalis/page.md
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/logger.js
+++ .github/workflows/logger.js
@@ -3,11 +3,12 @@ import pino from 'pino'
 const logger = pino({
   level: process.env.PINO_LOG_LEVEL || 'debug',
   transport: {
-    target: './pino-pretty-transport',
+    target: 'pino-pretty',
     options: {
       colorize: true,
     },
   },
+  messageFormat: 'chafeel {levelLabel} - {pid} - url:{req.url}',
 })
 
 export default logger
