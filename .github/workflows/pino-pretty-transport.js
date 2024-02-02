42b25541b96cfc476a07fb154ae07e26a8162a5a
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-02-02 20:30:44 +0000

    [CK-Broadcast] src/app/articles/alopecia-universalis/page.md
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/pino-pretty-transport.js
+++ .github/workflows/pino-pretty-transport.js
@@ -7,6 +7,6 @@ export default (opts) =>
     //messageFormat: (log, messageKey) => `hello ${log[messageKey]}`,
     messageFormat: (log, messageKey, levelLabel) => {
       // do some log message customization
-      return `hello dawg ${log[messageKey]} ${log[levelLabel]} ${log.levelLabel} ${log.pid} ${log.req.url} ${log.req.method} ${log.req.headers['user-agent']} ${log.res.statusCode} ${log.res.headers['content-type']}`
+      return `hello dawg ${log[messageKey]} ${log[levelLabel]} ${log.levelLabel} ${log.pid}`
     },
   })
