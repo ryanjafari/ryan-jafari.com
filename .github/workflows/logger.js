45c8e93e39d3f6023be9982531ec9623c96b8fb7
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-02-02 20:27:25 +0000

    [CK-Broadcast] src/app/articles/alopecia-universalis/page.md

--- .github/workflows/logger.js
+++ .github/workflows/logger.js
@@ -3,12 +3,11 @@ import pino from 'pino'
 const logger = pino({
   level: process.env.PINO_LOG_LEVEL || 'debug',
   transport: {
-    target: 'pino-pretty',
+    target: './pino-pretty-transport',
     options: {
       colorize: true,
     },
   },
-  messageFormat: 'chafeel {levelLabel} - {pid} - url:{req.url}',
 })
 
 export default logger
--- .github/workflows/pino-pretty-transport.js
+++ .github/workflows/pino-pretty-transport.js
@@ -3,6 +3,10 @@ import pinoPretty from 'pino-pretty'
 export default (opts) =>
   pinoPretty({
     ...opts,
-    messageFormat: 'chafeel {levelLabel} - {pid} - url:{req.url}',
+    //messageFormat: 'chafeel {levelLabel} - {pid} - url:{req.url}',
     //messageFormat: (log, messageKey) => `hello ${log[messageKey]}`,
+    messageFormat: (log, messageKey, levelLabel) => {
+      // do some log message customization
+      return `hello dawg ${log[messageKey]} ${levelLabel}`
+    },
   })
