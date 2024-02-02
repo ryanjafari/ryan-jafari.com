00b38ed9bd05e93a83fbb03d3daf1d2b2d35fb1b
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-02-02 20:10:30 +0000

    [CK-Broadcast] src/app/articles/alopecia-universalis/page.md
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/pino-pretty-transport.js
+++ .github/workflows/pino-pretty-transport.js
@@ -3,6 +3,6 @@ import pinoPretty from 'pino-pretty'
 export default (opts) =>
   pinoPretty({
     ...opts,
-    messageFormat: 'chafeel {level} - {if pid}{pid} - {end}url:{req.url}',
+    messageFormat: 'chafeel {logLevel} - {if pid}{pid} - {end}url:{req.url}',
     //messageFormat: (log, messageKey) => `hello ${log[messageKey]}`,
   })
