ab4e09d66e5520d0dc5cf625c7a0d7ab7ecab386
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-02-07 17:06:41 +0000

    [CK-Broadcast] src/app/articles/alopecia-universalis/page.md
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/parseFrontMatter.js
+++ .github/workflows/parseFrontMatter.js
@@ -1,7 +1,5 @@
-import chalk from 'chalk'
 import fsp from 'fs/promises'
 import yaml from 'js-yaml'
-import { customLog } from './customLog.js'
 import createFileLogger from './logger.js'
 
 const fileLogger = createFileLogger(import.meta.url)
@@ -13,12 +11,12 @@ export default async function parseFrontMatter() {
   log.info('Parsing article front matter...')
 
   const { ARTICLE_PATH } = process.env
-  customLog(chalk.blue('article path env:'), ARTICLE_PATH)
+  log.debug({ ARTICLE_PATH })
 
   const content = await fsp.readFile(ARTICLE_PATH, 'utf8')
   const parts = content.split('---')
   const frontMatter = yaml.load(parts[1])
-  customLog(chalk.blue('article front matter:'), frontMatter)
+  log.debug({ frontMatter })
 
   return frontMatter
 }
