44cc60940b8ca68ec2862281faeb5d9f6f95b44f
Ryan Jafari <29539+ryanjafari@users.noreply.github.com>
2024-02-11 01:40:33 +0000

    [CK-Broadcast] src/app/articles/alopecia-universalis/page.md
    Signed-off-by: Ryan Jafari <29539+ryanjafari@users.noreply.github.com>

--- .github/workflows/sendCkEmail.js
+++ .github/workflows/sendCkEmail.js
@@ -62,33 +62,28 @@ const postEmailToConvertKit = async (envVars, { content, subject }) => {
 
 // Main function orchestrating the email sending process
 const main = async () => {
-  log.debug('Initiating email sending process to ConvertKit.')
-  const envVars = parseEnvVariables()
-  const frontMatter = JSON.parse(envVars.ARTICLE_FRONT_MATTER)
-  const { url, content, subject } = prepareEmailContent(
-    envVars.ARTICLE_PATH,
-    envVars.NEXT_PUBLIC_SITE_URL,
-    frontMatter,
-  )
+  try {
+    log.debug('Initiating email sending process to ConvertKit.')
+    const envVars = parseEnvVariables()
+    const frontMatter = JSON.parse(envVars.ARTICLE_FRONT_MATTER)
+    const { url, content, subject } = prepareEmailContent(
+      envVars.ARTICLE_PATH,
+      envVars.NEXT_PUBLIC_SITE_URL,
+      frontMatter,
+    )
 
-  saveToGitHubOutput('articleUrl', url)
+    saveToGitHubOutput('articleUrl', url)
 
-  const response = await postEmailToConvertKit(envVars, { content, subject })
-  log.debug({ response }, 'Received response from ConvertKit.')
-  const responseBody = await parseResponse(response)
-  log.debug({ responseBody }, 'Parsed response body from ConvertKit.')
+    const response = await postEmailToConvertKit(envVars, { content, subject })
+    log.debug({ response }, 'Received response from ConvertKit.')
+    const responseBody = await parseResponse(response)
+    log.debug({ responseBody }, 'Parsed response body from ConvertKit.')
 
-  log.info({ url, subject }, 'Email successfully sent to ConvertKit.')
+    log.info({ url, subject }, 'Email successfully sent to ConvertKit.')
+  } catch (error) {
+    log.error({ error }, 'An error occurred during the email sending process.')
+    process.exit(1)
+  }
 }
 
 main()
-  .then(() => {
-    log.info('Email sending process completed.')
-  })
-  .catch((error) => {
-    log.error({ error }, 'An error occurred during the email sending process.')
-    process.exit(1)
-  })
-  .finally(() => {
-    log.info('Finished sending email.')
-  })
